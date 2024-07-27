d1658e174276e7882749c0b2b9d6ca369f868aa8
juancarlosmaya <juancarlosmayag@gmail.com>
2024-07-27 17:10:17 -0500

    inicio de proyecto

---
+++ trazabilidad_medicamentos/trazabilidad_medicamentos/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for trazabilidad_medicamentos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trazabilidad_medicamentos.settings')
+
+application = get_asgi_application()
